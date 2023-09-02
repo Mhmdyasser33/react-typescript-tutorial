5e6ca5b9eb1c92d64d710caed001f568827231fc
mohamed yasser <111688773+Mhmdyasser33@users.noreply.github.com>
2023-09-02 03:07:25 -0700

    dealing ith array of object

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,26 @@ import './App.css'
 import SayHello from './components/SayHello'
 
 function App() {
-  const userData = {
-    name :"mohamed" ,
-    age : 21 ,
-    login : true ,
-  }
+  const loginUserDetails = [
+    {
+      name : "mohamed" ,
+      age : 21 ,
+      login : true ,
+    },
+    {
+      name : "ahmed" ,
+      age : 20 ,
+      login : false ,
+    },
+    {
+      name : "ali" ,
+      age : 19 ,
+      login : true ,
+    },
+  ]
   return (
 <div className='container'>
- <SayHello userInfo={userData} />
+ <SayHello loginUserDetails = {loginUserDetails} />
 </div>
   )
 }
--- src/components/SayHello.tsx
+++ src/components/SayHello.tsx
@@ -1,16 +1,21 @@
 type Props = {
-   userInfo : {
-    name : string ;
-    age : number ;
-    login : boolean ;
-   }
+    loginUserDetails : {
+      name : string ,
+      age : number ,
+      login :boolean
+    }[]
 }
 
-const SayHello = ({userInfo} : Props) => {
+const SayHello = ({loginUserDetails} : Props) => {
   return (
     <>
-   <h1>{userInfo.name ? `hello ${userInfo.name}` : "hello Guest"}</h1>
-   <h2>My age is {userInfo.age}</h2>
+      <ul>
+         {loginUserDetails.map((item , index) =>(
+      <li key={index}>
+         {item.name} ----------- {item.age}
+      </li>
+         ))}
+      </ul>
     </>
   )
 }
