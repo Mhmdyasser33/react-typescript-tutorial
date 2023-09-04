b286f069c4a6f675a1b78a3c2785cc07138bf921
mohamed yasser <111688773+Mhmdyasser33@users.noreply.github.com>
2023-09-04 03:33:03 -0700

    add important comment

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,7 +4,9 @@ type btnProps = {
      // ? dealing with event and specify type of event here.., mouseEvent here specify the event that user will to and <> this generics specify the element that user will use like button element or input and so on...
     handleClick : (e : React.MouseEvent<HTMLButtonElement>)=> void ;
 }
+
 const Button = ({handleClick} : btnProps) => {
+    // ! take care here use using without using listening () => function name , this is used when a function take more than one argument
   return <button onClick={handleClick}> CLick Me!</button>
 
 }
